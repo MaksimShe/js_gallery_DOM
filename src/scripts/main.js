16dfa73cbac5c6d77fe4c9a28c77584a575f7a49
SheFing <shevchukm2002@gmail.com>
2025-10-08 18:13:02 +0300

    ai buddy is gay1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+/* eslint-disable function-paren-newline */
 'use strict';
+
+const deleteRedirect = document.querySelectorAll('.list-item__link');
+const arrLargePhoto = [...deleteRedirect].map((i) => i.href);
+
+deleteRedirect.forEach((i) => i.removeAttribute('href'));
+
+const imgClick = document.querySelectorAll('.list-item');
+
+imgClick.forEach((item, index) =>
+  item.addEventListener('click', () => mainImg(item, index)),
+);
+
+function mainImg(item, index) {
+  const mainImgNow = document.querySelector('#largeImg');
+
+  mainImgNow.setAttribute('src', arrLargePhoto[index]);
+}
